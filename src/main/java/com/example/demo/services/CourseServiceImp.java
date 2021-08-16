46420e82d586f0f305c50c1165d329346527814f
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-08-16 23:16:55 +0600

    initial commit

--- src/main/java/com/example/demo/services/CourseServiceImp.java
+++ src/main/java/com/example/demo/services/CourseServiceImp.java
@@ -1,46 +1,55 @@
 package com.example.demo.services;
 
+import com.example.demo.dao.CourseDao;
 import com.example.demo.entities.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 @Service
 public class CourseServiceImp implements CourseService{
-    private List<Course> list;
+ //   private List<Course> list;
+    @Autowired
+    private CourseDao courseDao;
 
     public CourseServiceImp() {
-        list = new ArrayList<>();
+        /*  list = new ArrayList<>();
         list.add(new Course(145,"Java Core Course","This course contain basic of Java"));
-        list.add(new Course(146,"Spring Boot Course","Creating restAPI using spring boot"));
+        list.add(new Course(146,"Spring Boot Course","Creating restAPI using spring boot"));*/
 
     }
 
     @Override
     public List<Course> getCourses() {
-        return list;
+       // return list;
+        return courseDao.findAll();
     }
 
     @Override
     public Course getCourse(long courseId) {
-        Course course = null;
+ /*       Course course = null;
         for(Course course1: list){
             if(course1.getId() == courseId){
                 course = course1;
             }
         }
-        return course;
+        return course;*/
+        return courseDao.getOne(courseId);
+
     }
 
     @Override
     public Course addCourse(Course course) {
-        list.add(course);
+       /* list.add(course);
+        return course;*/
+        courseDao.save(course);
         return course;
     }
 
     @Override
     public Course updateCourse(Course course) {
-        Course course1 = null;
+/*        Course course1 = null;
         for(Course c: list){
             if(c.getId() == course.getId()){
                 c.setTitle(course.getTitle());
@@ -48,16 +57,20 @@ public class CourseServiceImp implements CourseService{
                 course1 = c;
             }
         }
-        return course1;
+        return course1;*/
+        courseDao.save(course);
+        return course;
     }
 
     @Override
     public void deleteCourse(long courseId) {
-        Course course;
+/*        Course course;
         for(Course c: list){
             if(c.getId() == courseId){
                 list.remove(c);
             }
-        }
+        }*/
+        Course entity = courseDao.getOne(courseId);
+        courseDao.delete(entity);
     }
 }
